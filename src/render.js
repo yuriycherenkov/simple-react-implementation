8237b356d2d9f1a1c94b4052e8ec8d16f816e97f
Yuriy Cherenkov <cherenkov.yuriy@nixsolutions.com>
2017-07-04 11:58:13 +0300

    start render

--- src/render.js
+++ src/render.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import { parseVDom } from './Component';
 
 let VDom = {};
@@ -12,15 +13,40 @@ export const render = (Obj, domElement) => {
     return true;
   };
 
-  VDom = Object.assign({}, Obj);
   // checkToRender();
+  let newObj;
+  const deepEqual = (x, y) => (
+    (x && y && typeof x === 'object' && typeof y === 'object') ?
+      (Object.keys(x).length === Object.keys(y).length) &&
+        Object.keys(x).reduce((isEqual, key) => {
+          if (key === 'props') {
+            console.log('isEqual ', isEqual, 'key ', key, 'x[key], y[key]: ', x[key], y[key]);
+          }
+          if (key === 'children') {
+             console.log('isEqual ', isEqual, 'key ', key, 'x[key], y[key]: ', x[key], y[key]);
+          }
+          return isEqual && deepEqual(x[key], y[key]);
+        }, true) : (x === y)
+  );
 
   if (Obj.render) {
+    newObj = _.merge(Obj.render(), { id: Obj.props.id });
     domElement.innerHTML = '';
-    const shouldBeShown = parseVDom(Obj.render());
+    // console.log(_.isEqual(VDom, newObj));
+    console.log('/-------------- start -----------------/');
+    deepEqual(VDom, newObj);
+    console.log('/--------------- end ----------------/');
+    const shouldBeShown = parseVDom(newObj);
     domElement.appendChild(shouldBeShown);
   } else {
+    newObj = _.merge({}, Obj);
+    VDom = _.merge({}, Obj);
+    console.log('/------------ start -------------------/');
+    deepEqual(newObj, VDom);
+    console.log('/------------- end ------------------/');
     const shouldBeShown = parseVDom(Obj);
     domElement.appendChild(shouldBeShown);
   }
+
+  console.log('newObj ', newObj, 'VDOm ', VDom);
 };
